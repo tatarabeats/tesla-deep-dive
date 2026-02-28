6f1a88228d1270bfc541d73d3454a0e99c6e4053
Shumpei Kihara <shunpei.kihara@gmail.com>
2026-03-01 08:26:58 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'tesla-dive-v2';
+const BASE_PATH = '/tesla-deep-dive/';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([BASE_PATH, BASE_PATH + 'index.html']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
