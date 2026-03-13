6e5cd3e4d95a46f51937406a68278a2e40fc1032
Shumpei Kihara <shunpei.kihara@gmail.com>
2026-03-13 23:54:10 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,36 +1,11 @@
-const CACHE_NAME = 'tesla-dive-v2';
-const BASE_PATH = '/tesla-deep-dive/';
-
-self.addEventListener('install', (event) => {
+// Unregister old service worker and clear all caches
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([BASE_PATH, BASE_PATH + 'index.html']);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        if (response.ok) {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, clone);
-          });
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister()),
   );
 });
